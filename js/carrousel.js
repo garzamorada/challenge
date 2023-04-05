37b330444a40d1ebc4746f51b9c680a18fc651da
Andres Allievi <garzamorada@gmail.com>
2023-04-05 13:13:37 -0300

    carrusel foreach

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,4 +1,4 @@
-function derecha() {
+/*function derecha() {
     const diapositivas = document.getElementsByClassName('diapositiva');
     let cantDiapisitivas = diapositivas.length;
     for (let i = 0; i < diapositivas.length; i++) {
@@ -25,4 +25,32 @@ function izquierda() {
         if (order > cantDiapisitivas) { order = 1; }
         diapositiva.style.setProperty('order', order);
     }
+}*/
+
+
+function derecha() {
+    const diapositivas = document.getElementsByClassName('diapositiva');
+    let cantDiapisitivas = diapositivas.length;
+    diapositivas.forEach(diapositiva => {
+        let elementStyle = window.getComputedStyle(diapositiva);
+        let order = elementStyle.getPropertyValue('order')
+        order = parseInt(order, 10);
+        order = order - 1
+        if (order == 0) { order = cantDiapisitivas; }
+        diapositiva.style.setProperty('order', order);
+    });
+}
+
+function izquierda() {
+    const diapositivas = document.getElementsByClassName('diapositiva');
+    console.log(diapositivas);
+    let cantDiapisitivas = diapositivas.length;
+    diapositivas.forEach(diapositiva => {
+        let elementStyle = window.getComputedStyle(diapositiva);
+        let order = elementStyle.getPropertyValue('order')
+        order = parseInt(order, 10);
+        order = order + 1
+        if (order > cantDiapisitivas) { order = 1; }
+        diapositiva.style.setProperty('order', order);
+    });
 }
